69604bc693c3262f7276844b61c030452c21c17f
Adel <85290866+TotalGenius@users.noreply.github.com>
2021-06-09 14:11:17 +0300

    created post methods in PriorityController and CategoryController and added Generated Value for id in Category and Priority classes

--- src/main/java/com/demo/tasklist/springbootbackend/controller/CategoryController.java
+++ src/main/java/com/demo/tasklist/springbootbackend/controller/CategoryController.java
@@ -3,6 +3,8 @@ package com.demo.tasklist.springbootbackend.controller;
 import com.demo.tasklist.springbootbackend.entity.Category;
 import com.demo.tasklist.springbootbackend.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,8 +23,22 @@ public class CategoryController {
     }
 
     @PostMapping("/add")
-    public void add(@RequestBody Category category) {
+    public ResponseEntity<Category> add(@RequestBody Category category) {
+
+        if (category.getId() != null) {
+            return new ResponseEntity("id must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (category.getTitle() == null || category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("title cannot be empty or consisted of spaces", HttpStatus.NOT_ACCEPTABLE);
+        }
+
         categoryRepository.save(category);
+
+
+        return new ResponseEntity("New category was added", HttpStatus.OK);
+
+
     }
 
 }
--- src/main/java/com/demo/tasklist/springbootbackend/controller/PriorityController.java
+++ src/main/java/com/demo/tasklist/springbootbackend/controller/PriorityController.java
@@ -3,6 +3,9 @@ package com.demo.tasklist.springbootbackend.controller;
 import com.demo.tasklist.springbootbackend.entity.Priority;
 import com.demo.tasklist.springbootbackend.repository.PriorityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +17,9 @@ public class PriorityController {
     @Autowired
     private PriorityRepository priorityRepository;
 
+    @Value("${priorityDefaultColor}")
+    private String defaultColor;
+
     @GetMapping("/test")
     public List<Priority> test() {
         List<Priority> list = priorityRepository.findAll();
@@ -21,8 +27,25 @@ public class PriorityController {
     }
 
     @PostMapping("/add")
-    public void add(@RequestBody Priority priority) {
+    public ResponseEntity<Priority> add(@RequestBody Priority priority) {
+
+        //Checking id (id must be null)
+        if (priority.getId() != null) {
+            return new ResponseEntity("id must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        //Checking title (title cannot be null or consists if spaces)
+        if (priority.getTitle() == null || priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("Title cannot be empty or consists of only spaces", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        //Checking color (if color empty set default color)
+        if (priority.getColor() == null || priority.getColor().trim().length() == 0) {
+            priority.setColor(defaultColor);
+        }
+
         priorityRepository.save(priority);
+        return new ResponseEntity("New Priority was added", HttpStatus.OK);
     }
 
 
