f88ae92622b54bc95e1cf8a020d5591b001a5921
Adel <85290866+TotalGenius@users.noreply.github.com>
2021-06-10 15:29:52 +0300

    Change StatController 2

--- src/main/java/com/demo/tasklist/springbootbackend/controller/StatController.java
+++ src/main/java/com/demo/tasklist/springbootbackend/controller/StatController.java
@@ -1,18 +1,14 @@
 package com.demo.tasklist.springbootbackend.controller;
 
-import com.demo.tasklist.springbootbackend.entity.Priority;
+
 import com.demo.tasklist.springbootbackend.entity.Stat;
 import com.demo.tasklist.springbootbackend.repository.StatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.NoSuchElementException;
 
 @RestController
 public class StatController {
